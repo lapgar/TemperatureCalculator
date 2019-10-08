3ae0fc1273053b0fba019ae0567c05da70669869
Lindsay Apgar <51244784+lapgar@users.noreply.github.com>
2019-10-07 20:00:30 -0400

    Minor cleanup
    Adding some white space for a cleaner look

--- figuringOutInput.py
+++ figuringOutInput.py
@@ -1,16 +1,16 @@
-#ask for the temp in F
+# ask for the temp in F
 f_temp = int(input("What is the temperature in fahrenheit?"))
 
-#convert f to c
-c_temp = (f_temp-32)*(5/9)
+# convert f to c
+c_temp = (f_temp - 32) * (5 / 9)
 
-#round to one decimal place
+# round to one decimal place
 answer = round(c_temp, 1)
 
-#tell user
+# tell user
 print(answer, "degrees celsius")
 
-#add a personal touch
+# add a personal touch
 if c_temp <= 0:
     print("Wow, freezing!")
 elif c_temp >= 100:
